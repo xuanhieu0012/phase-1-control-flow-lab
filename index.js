647effe8a2ad7e9b7e6efefdf722cf43128f5922
Hieu <xuanhieu0012@gmail.com>
2021-08-09 12:13:54 -0500

    Complete

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(number){
   // Write your code here!
+  if (number <= 400){
+    return "This one is on me!";
+  }
+  else if (number  > 2000 && number < 2499){
+    return "I will gladly take your thirty bucks.";
+  }
+  else if (number > 2500){
+    return "No can do."
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city ==="NYC"? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.'
+  }
 }
